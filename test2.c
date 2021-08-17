89f392bd464722b8c3a411c3b7581757f6243c37
devanshu <devanshukumar45@gmail.com>
2021-08-17 14:16:51 +0530

    changed sort order

--- test2.c
+++ test2.c
@@ -52,7 +52,7 @@ int binSearch(int* arr, int element, int len){
 int* bubbleSort(int* arr, int len){
     for(int i = 0; i < len-1; i++){
         for(int j = 0; j < len-1; j++){
-            if(arr[j] < arr[j+1]){
+            if(arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
