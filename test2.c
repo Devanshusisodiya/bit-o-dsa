63808f5dd29ad5e3ade8cbce34e9f2722c6557cc
devanshu <devanshukumar45@gmail.com>
2021-08-17 13:53:30 +0530

    updates

--- test2.c
+++ test2.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void traverse(int* arr, int len){
+    for(int i = 0; i < len; i++){
+        printf("%d\n", arr[i]);
+    }
+    printf("\n");
+}
+
 int* delete(int *arr, int element, int index, int len){
     int i = 0;
     for(int j = 0; j < len; j++){
@@ -9,6 +16,46 @@ int* delete(int *arr, int element, int index, int len){
         }else{
             arr[i] = arr[j];
             i += 1;
+        }
+    }
+    return arr;
+}
+
+int linSearch(int* arr, int element, int len){
+    int index = -1;
+    for(int i = 0; i < len; i++){
+        if(arr[i] == element){
+            index = i;
+        }
+    }
+    return index;
+}
+
+int binSearch(int* arr, int element, int len){
+    int index = -1;
+    int low = 0;
+    int high = len;
+    while(low <= high){
+        int mid = (low + high)/2;
+        if(element < arr[mid]){
+            mid = high - 1;
+        }if(element > arr[mid]){
+            mid = low + 1;
+        }if(element == arr[mid]){
+            index = mid;
+        }
+    }
+    return index;
+}
+
+int* bubbleSort(int* arr, int len){
+    for(int i = 0; i < len-1; i++){
+        for(int j = 0; j < len-1; j++){
+            if(arr[j] < arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
     }
     return arr;
@@ -24,23 +71,8 @@ void main(){
         scanf("%d", &arr[i]);
     }
     printf("\n");
-    
-    for(int i = 0; i < len; i++){
-        printf("%d", arr[i]);
-    }
-    printf("\n");
 
-
-    int* new = delete(arr, 2, 1, 4);
-
-    // int arr[] = {1,2,3,4};
-    // int* new = delete(arr, 2, 1, 4);
-    // printf("compiled\n");
-
-
-    for (int i = 0; i < 3; i++){
-        printf("%d\n", new[i]);
-    }
+    // IMPLEMENTING MENU DRIVEN 
     
 
 }
