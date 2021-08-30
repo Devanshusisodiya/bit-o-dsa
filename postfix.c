e789ba6a6ddcf4951a1d0663ad7f3efa79230230
devanshu <devanshukumar45@gmail.com>
2021-08-30 10:13:18 +0530

    postfix evaluation

--- postfix.c
+++ postfix.c
@@ -1,54 +1,144 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct stack{
-    int size;
+struct stack {
     int top;
-    int* arr;
+    int maxSize;
+    int *arr;
 };
 
-struct stack* createStack(int size) {
-    struct stack* stack = (struct stack*)malloc(sizeof(struct stack));
-    stack->size = size;
-    stack->top = -1;
-    return stack;
+struct stack* createStack(int size){
+    struct stack* ptr = (struct stack*)malloc(sizeof(struct stack));
+
+    ptr->top = -1;
+    ptr->maxSize = size;
+    ptr->arr = (int*)malloc(size*sizeof(int));
+
+    return ptr;
 }
 
-int isFull(struct stack* stack) {
-    if(stack->top == stack->size-1){
+int isFull(struct stack* s){
+    if(s->top == s->maxSize - 1){
         return 1;
     }else{
         return 0;
     }
 }
 
-int isEmpty(struct stack* stack) {
-    if(stack->top == -1){
+int isEmpty(struct stack* s){
+    if(s->top == -1){
         return 1;
     }else{
         return 0;
     }
 }
 
-void push(struct stack* stack, int element) {
-    if(isFull(stack) == 1){
+void push(struct stack* s, int element){
+    if(isFull(s) == 1){
         printf("overflow");
     }else{
-        stack->top = stack->top + 1;
-        stack->arr[stack->top] = element;
+        s->top = s->top + 1;
+        s->arr[s->top] = element;
     }
 }
 
-int pop(struct stack* stack) {
-    if(isEmpty == 1){
-        printf("underflow");
+int pop(struct stack* s){
+    if(isEmpty(s)){
+        printf("stack empty");
         return -1;
     }else{
-        stack->top = stack->top - 1;
-        return stack->arr[stack->top];
+        int popped = s->arr[s->top];
+        s->top = s->top - 1;
+        return popped;
     }
+}
+
+int peek(struct stack* stack) {
+    return stack->arr[stack->top];
+}
+
+void traverse(struct stack* stack){
+    printf("\n\ncurrent stack\n");
+    for(int i = 0; i < stack->top + 1; i++){
+        printf("%d\n", stack->arr[i]);
+    }
+    printf("\n\n");
 }
 
 void main() {
-    
+    int size;
+    printf("enter size of stack : ");
+    scanf("%d", &size);
+
+    struct stack* s = createStack(size);
+    printf("size of stack is : %d", s->maxSize);
+
+    printf("\n\ncompiled\n\n");
+
+    // postfix operation
+    int ctr = 0, choice;
+    do{
+        int operand;
+        char operator[1];
+       
+        printf("1. operand\n");
+        printf("2. operator\n");
+        printf("3. traverse\n");
+        printf("4. eval\n");
+        printf("5. quit\n");
+        scanf("%d", &choice);
+
+        int a = 0, b = 0, c = 0;
+
+        switch (choice){
+        case 1:
+            printf("enter operand : ");
+            scanf("%d", &operand);
+            push(s, operand);
+            break;
+        case 2:
+            printf("enter operator : ");
+            scanf("%s", operator);
+            if(operator[0] == '+'){
+                b = pop(s);
+                a = pop(s);
+                c = a + b;
+                // printf("%d %d %d\n", c, a, b);
+                push(s, c);
+
+            }else if(operator[0] == '-'){
+                b = pop(s);
+                a = pop(s);
+                c = a - b;
+                push(s, c);
+
+            }else if(operator[0] == '*') {
+                b = pop(s);
+                a = pop(s);
+                c = a * b;
+                push(s, c);
+
+            }else if(operator[0] == '/') {
+                b = pop(s);
+                a = pop(s);
+                c = a / b;
+                push(s, c);
+
+            }else{
+                printf("operator not valid\n");
+            }
+            break;
+        case 3:
+            traverse(s);
+            break;
+        case 4:
+            printf("\n\ncurrent evaluation : %d\n\n", s->arr[s->top]);
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            break;
+        }
+    }while(ctr < 100);
 }
--- stack2.c
+++ stack2.c
@@ -47,9 +47,22 @@ int pop(struct stack* s){
         printf("stack empty");
         return -1;
     }else{
+        int popped = s->arr[s->top];
         s->top = s->top - 1;
-        return s->arr[s->top];
+        return popped;
     }
+}
+
+int peek(struct stack* stack) {
+    return stack->arr[stack->top];
+}
+
+void traverse(struct stack* stack){
+    printf("\n\ncurrent stack\n");
+    for(int i = 0; i < stack->top + 1; i++){
+        printf("%d\n", stack->arr[i]);
+    }
+    printf("\n\n");
 }
 
 void main() {
@@ -62,23 +75,70 @@ void main() {
 
     printf("\n\ncompiled\n\n");
 
-    // taking stack input
-    for(int i = 0; i < size; i++){
-        int element;
-        printf("enter : ");
-        scanf("%d", &element);
-        push(s, element);
-    }
-    printf("\n");
+    // postfix operation
+    int ctr = 0, choice;
+    do{
+        int operand;
+        char operator[1];
+       
+        printf("1. operand\n");
+        printf("2. operator\n");
+        printf("3. traverse\n");
+        printf("4. eval\n");
+        printf("5. quit\n");
+        scanf("%d", &choice);
 
-    //displaying stack
-    for(int i = 0; i < s->top+1; i++){
-        printf("%d", s->arr[i]);
-    }
-    printf("\n");
+        int a = 0, b = 0, c = 0;
 
-    //overflow
-    push(s, 69);     // :)
+        switch (choice){
+        case 1:
+            printf("enter operand : ");
+            scanf("%d", &operand);
+            push(s, operand);
+            break;
+        case 2:
+            printf("enter operator : ");
+            scanf("%s", operator);
+            if(operator[0] == '+'){
+                b = pop(s);
+                a = pop(s);
+                c = a + b;
+                // printf("%d %d %d\n", c, a, b);
+                push(s, c);
 
+            }else if(operator[0] == '-'){
+                b = pop(s);
+                a = pop(s);
+                c = a - b;
+                push(s, c);
 
+            }else if(operator[0] == '*') {
+                b = pop(s);
+                a = pop(s);
+                c = a * b;
+                push(s, c);
+
+            }else if(operator[0] == '/') {
+                b = pop(s);
+                a = pop(s);
+                c = a / b;
+                push(s, c);
+
+            }else{
+                printf("operator not valid\n");
+            }
+            break;
+        case 3:
+            traverse(s);
+            break;
+        case 4:
+            printf("\n\ncurrent evaluation : %d\n\n", s->arr[s->top]);
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            break;
+        }
+    }while(ctr < 100);
 }
